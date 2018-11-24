44c23d86578cc60442811bfa73ed57dfa3533cad
siyoon <jung_210@naver.com>
2018-11-24 16:58:55 +0900

    feat: 빈설정 XML에제 자바 Configuration과 bean애노테이션으로 이용하여 설정

--- fastcampus/studentMS/src/main/java/exercise/studentms/Main.java
+++ fastcampus/studentMS/src/main/java/exercise/studentms/Main.java
@@ -1,18 +1,19 @@
 package exercise.studentms;
 
+import exercise.studentms.config.StudentMS;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("studentMS.xml");
-        MainController mc = ctx.getBean("mainController", MainController.class);
-        mc.control();
-
-        //ApplicationContext ctx2 = new AnnotationConfigApplicationContext(exercise.studentms.StudnetMSConfig.class);
-//        MainController mc = ctx2.getBean("mainController", MainController.class);
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("studentMS.xml");
+//        MainController mc = ctx.getBean("mainController", MainController.class);
 //        mc.control();
+
+        ApplicationContext ctx2 = new AnnotationConfigApplicationContext(StudentMS.class);
+        MainController mc2 = ctx2.getBean("mainController", MainController.class);
+        mc2.control();
     }
 }
 
