3c66d83ad84feceab8f59bab1815744edfbcc51c
siyoon210 <siyoon210@gmail.com>
2019-03-30 14:41:38 +0900

    3.3.2 로컬클래스 -> 익명클래스 -> 람다를 활용하여서 리펙토링

--- toby/vol1/src/springbook/user/dao/UserDao.java
+++ toby/vol1/src/springbook/user/dao/UserDao.java
@@ -17,8 +17,16 @@ public class UserDao {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy((Connection c) -> {
+                    PreparedStatement ps =
+                            c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
+                    ps.setString(1, user.getId());
+                    ps.setString(2, user.getName());
+                    ps.setString(3, user.getPassword());
+
+                    return ps;
+                }
+        );
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -43,8 +51,9 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy((Connection c) ->
+                c.prepareStatement("delete from users"));
+
     }
 
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
