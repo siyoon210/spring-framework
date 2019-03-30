0d0df69107e7e299b7ac93c7f9b5189a1385118b
siyoon210 <siyoon210@gmail.com>
2019-03-30 12:44:58 +0900

    1.5.1 스프링을 이용하여서 애플리케이션 컨텍스트로 의존주입하기

--- toby/vol1/src/springbook/user/Main.java
+++ toby/vol1/src/springbook/user/Main.java
@@ -1,5 +1,7 @@
 package springbook.user;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -8,7 +10,9 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("siyoon");
--- toby/vol1/src/springbook/user/dao/DaoFactory.java
+++ toby/vol1/src/springbook/user/dao/DaoFactory.java
@@ -1,18 +1,16 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-//    public AccountDao accountDao() {
-//        return new AccountDao(connectionMaker());
-//    }
-//
-//    public MessageDao messageDao() {
-//        return new MessageDao(connectionMaker());
-//    }
-
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
