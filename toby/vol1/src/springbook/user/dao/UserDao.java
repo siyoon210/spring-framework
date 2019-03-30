5898b5798b988d12466a2294b8cb2de126ee9841
siyoon210 <siyoon210@gmail.com>
2019-03-30 11:32:11 +0900

    1.1장 초난감 DAO DB에서 User를 조회하고 추가하는 기능을 만들었다. 기능은 정상적으로 수행되지만 아주 초난감한 상태에 코드다.

---
+++ toby/vol1/src/springbook/user/dao/UserDao.java
@@ -0,0 +1,45 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException{
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "spring", "book");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
