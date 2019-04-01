d6b3e47ee30cf0a497a6715e2c094bfe5e05a435
siyoon210 <siyoon210@gmail.com>
2019-04-01 17:16:14 +0900

    3장 마무리, 스프링에 내장된 jdbcTemplate을 활용하여 리팩토링

--- toby/vol1/src/springbook/user/dao/UserDao.java
+++ toby/vol1/src/springbook/user/dao/UserDao.java
@@ -1,62 +1,52 @@
 package springbook.user.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class UserDao {
-    private JdbcContext jdbcContext;
-    private org.springframework.jdbc.datasource.SimpleDriverDataSource datasource;
-
-    public void setDatasource(SimpleDriverDataSource datasource) {
-        this.datasource = datasource;
+    public void setDatasource(DataSource datasource) {
+        this.jdbcTemplate = new JdbcTemplate(datasource);
     }
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
+    private JdbcTemplate jdbcTemplate;
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        this.jdbcContext.workWithStatementStrategy((Connection c) -> {
-                    PreparedStatement ps =
-                            c.prepareStatement("insert into users(id,name,password) values (?,?,?)");
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-
-                    return ps;
-                }
-        );
-    }
-
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = datasource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
+    private RowMapper<User> userMapper = (ResultSet rs, int i) -> {
         User user = new User();
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
 
-        rs.close();
-        ps.close();
-        c.close();
-
         return user;
+    };
+
+    public void add(final User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password)" +
+                "values (?,?,?)", user.getId(), user.getName(), user.getPassword());
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                new Object[]{id}, this.userMapper
+        );
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id",
+                this.userMapper);
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+        this.jdbcTemplate.update("delete from users");
     }
 
+    public int getCount() {
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
+    }
 }
