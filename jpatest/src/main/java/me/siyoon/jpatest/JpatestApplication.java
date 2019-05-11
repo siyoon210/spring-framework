447250e89d98ad4c2e17011fca57761e4eb372f2
siyoon <siyoon210@gmail.com>
2019-05-11 11:38:10 +0900

    스프링 데이터 jpa 연습용 프로젝트 생성

---
+++ jpatest/src/main/java/me/siyoon/jpatest/JpatestApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.jpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpatestApplication.class, args);
+    }
+
+}
