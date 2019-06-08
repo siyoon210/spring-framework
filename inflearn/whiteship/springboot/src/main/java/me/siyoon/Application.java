db2dcd10e82dfef8eaad8e8c5300ecce534d6896
siyoon <siyoon210@gmail.com>
2019-06-08 14:43:46 +0900

    application.properties를 활용하여서 자동 설정된 빈을 뚝딱 만들기! 성공

--- inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
@@ -3,7 +3,6 @@ package me.siyoon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,13 +11,5 @@ public class Application {
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
-    }
-
-    @Bean
-    public SingleMan singleMan() {
-        SingleMan singleMan = new SingleMan();
-        singleMan.setName("gildong");
-        singleMan.setHowLongDays(300);
-        return singleMan;
     }
 }
