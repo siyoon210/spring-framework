4829d881432c215d884fac363ca3d723f29d00e9
siyoon <siyoon210@gmail.com>
2019-06-08 14:28:26 +0900

    @ComponentScan단계에서 만든 빈이 설정되도록 하기 성공

--- inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
@@ -3,6 +3,7 @@ package me.siyoon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -11,5 +12,13 @@ public class Application {
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
+    }
+
+    @Bean
+    public SingleMan singleMan() {
+        SingleMan singleMan = new SingleMan();
+        singleMan.setName("gildong");
+        singleMan.setHowLongDays(300);
+        return singleMan;
     }
 }
