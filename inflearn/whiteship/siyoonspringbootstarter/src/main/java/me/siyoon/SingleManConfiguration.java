36a003198ecf38101f142b58169ea01b1149d867
siyoon <siyoon210@gmail.com>
2019-06-08 14:27:41 +0900

    @ConditionalOnMissingBean을 이용하여서 @ComponenetScan으로 설정된 빈이 없는 경우에만 만들도록 설정

--- inflearn/whiteship/siyoonspringbootstarter/src/main/java/me/siyoon/SingleManConfiguration.java
+++ inflearn/whiteship/siyoonspringbootstarter/src/main/java/me/siyoon/SingleManConfiguration.java
@@ -1,11 +1,13 @@
 package me.siyoon;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SingleManConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     public SingleMan singleMan() {
         SingleMan singleMan = new SingleMan();
         singleMan.setName("siyoon");
