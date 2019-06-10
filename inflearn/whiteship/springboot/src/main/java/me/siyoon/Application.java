fb43ff629f2e03e22795eeec93a5085f4df88dfe
siyoon <siyoon210@gmail.com>
2019-06-10 22:06:47 +0900

    SpringApplication의 setBanner메소드를 사용하여서 배너 설정해보기

--- inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/Application.java
@@ -1,15 +1,14 @@
 package me.siyoon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.setBanner((environment, sourceClass, out) -> System.out.println("뚜비두밤~"));
         springApplication.run(args);
+
     }
 }
