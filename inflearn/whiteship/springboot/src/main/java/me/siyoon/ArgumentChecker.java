020017c19fd5a28bbf25f410758c9468a7be616b
siyoon <siyoon210@gmail.com>
2019-06-10 22:36:43 +0900

    ApplicationRunner 인터페이스를 이용해서 program Args 가져오기
    이 또한 VM Option은 읽지 못했다.

--- inflearn/whiteship/springboot/src/main/java/me/siyoon/ArgumentChecker.java
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/ArgumentChecker.java
@@ -1,13 +1,14 @@
 package me.siyoon;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ArgumentChecker {
-    //빈의 생성자가 한개이고, 매개변수로 되어있는 것이 빈이라면 자동으로 주입해준다.
-    public ArgumentChecker(ApplicationArguments arguments) {
-        System.out.println("arguments.containsOption(\"foo\") = " + arguments.containsOption("foo"));
-        System.out.println("arguments.containsOption(\"bar\") = " + arguments.containsOption("bar"));
+public class ArgumentChecker implements ApplicationRunner {
+    @Override
+    public void run(final ApplicationArguments args) throws Exception {
+        System.out.println("args.getOptionNames().contains(\"foo\") = " + args.getOptionNames().contains("foo"));
+        System.out.println("args.getOptionNames().contains(\"bar\") = " + args.getOptionNames().contains("bar"));
     }
 }
