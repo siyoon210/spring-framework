27545c4f48a064b49180836ab96712fccc4c5bb1
siyoon <siyoon210@gmail.com>
2019-06-10 23:26:35 +0900

    @ConfigurationProperties를 이용하여서 application.properties의 값 읽어오기

---
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/MyProperties.java
@@ -0,0 +1,36 @@
+package me.siyoon;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("siyoon")
+public class MyProperties   {
+    private String name;
+    private int level;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(final int level) {
+        this.level = level;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(final String job) {
+        this.job = job;
+    }
+}
--- inflearn/whiteship/springboot/src/main/java/me/siyoon/SampleListener.java
+++ inflearn/whiteship/springboot/src/main/java/me/siyoon/SampleListener.java
@@ -1,14 +1,21 @@
 package me.siyoon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleListener implements ApplicationListener<ApplicationStartedEvent> {
+    @Autowired
+    private MyProperties properties;
 
     @Override
     public void onApplicationEvent(final ApplicationStartedEvent applicationStartedEvent) {
         System.out.println("ApplicationStartedEvent가 감지되었다!");
+
+        System.out.println("properties.getJob() = " + properties.getJob());
+        System.out.println("properties.getName() = " + properties.getName());
+        System.out.println("properties.getLevel() = " + properties.getLevel());
     }
 }
