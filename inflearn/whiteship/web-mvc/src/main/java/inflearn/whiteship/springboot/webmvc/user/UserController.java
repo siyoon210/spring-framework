d764af666038d636a21217d183566a19266d9dba
siyoon <siyoon210@gmail.com>
2019-06-13 22:56:12 +0900

    새로운 프로젝트 생성 + 테스트 코드 작성
    부트가 기본적인 설정을 다 해주었기에 바로 컨트롤러를 만들고 테스트 해볼수 있었다.
    WebMvc 설정을 커스터마이징 하고 싶다면 WebMvcConfigure를 구현한 설정 빈을 만들자.

---
+++ inflearn/whiteship/web-mvc/src/main/java/inflearn/whiteship/springboot/webmvc/user/UserController.java
@@ -0,0 +1,12 @@
+package inflearn.whiteship.springboot.webmvc.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ inflearn/whiteship/web-mvc/src/test/java/inflearn/whiteship/springboot/webmvc/user/UserControllerTest.java
@@ -0,0 +1,26 @@
+package inflearn.whiteship.springboot.webmvc.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+}
