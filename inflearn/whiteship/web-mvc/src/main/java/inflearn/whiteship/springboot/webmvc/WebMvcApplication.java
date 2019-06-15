1f55691d5f2a16634fe8fd041351a656cca376cd
siyoon <siyoon210@gmail.com>
2019-06-15 12:35:12 +0900

    webjars-locator-core 의존성을 넣으면 /webjars/.. 경로에 버전 명시를 생략가능하다.

---
+++ inflearn/whiteship/web-mvc/src/main/java/inflearn/whiteship/springboot/webmvc/WebMvcApplication.java
@@ -0,0 +1,13 @@
+package inflearn.whiteship.springboot.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcApplication.class, args);
+    }
+
+}
