9eed8f33bd451c48f717d5b01eba2151ec38e4b6
siyoon <siyoon210@gmail.com>
2019-06-16 16:21:00 +0900

    인프런 백기선 스프링부트 강좌 - 스프링 데이터 JPA용 프로젝트 생성

---
+++ inflearn/whiteship/spring-data-jpa/src/test/java/inflearn/whiteship/springboot/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package inflearn.whiteship.springboot.springdatajpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
