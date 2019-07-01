1909477ded97736db6532ae7e54e5902e63212f6
siyoon <siyoon210@gmail.com>
2019-07-01 22:29:30 +0900

    백기선 스프링 부트 강좌 - 스프링 시큐리티 프로젝트 생성

---
+++ inflearn/whiteship/spring-security/src/main/java/me/siyoon/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
