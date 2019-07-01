c1dd081efc75eaefb20db8200cbff5192a1b1a6f
siyoon <siyoon210@gmail.com>
2019-07-01 22:41:42 +0900

    MainController 테스트파일 작성

---
+++ inflearn/whiteship/spring-security/src/test/java/me/siyoon/springsecurity/MainControllerTest.java
@@ -0,0 +1,27 @@
+package me.siyoon.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+
+}
