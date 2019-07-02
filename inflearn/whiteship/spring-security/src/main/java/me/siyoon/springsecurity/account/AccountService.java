ff67256fee867c28a9285ff7740b993573fc454d
siyoon <siyoon210@gmail.com>
2019-07-02 23:06:02 +0900

    UserDetailsService를 구현함으로써 account를 스프링시큐리티가 알수 있는 유저디테일로 바꾸기

--- inflearn/whiteship/spring-security/src/main/java/me/siyoon/springsecurity/account/AccountService.java
+++ inflearn/whiteship/spring-security/src/main/java/me/siyoon/springsecurity/account/AccountService.java
@@ -1,11 +1,21 @@
 package me.siyoon.springsecurity.account;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements UserDetailsService {
     private final AccountRepository accountRepository;
 
     public Account createAccount(String username, String password) {
@@ -14,5 +24,16 @@ public class AccountService {
         account.setPassword(password);
 
         return accountRepository.save(account);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        Optional<Account> optionalAccount = accountRepository.findByUsername(username);
+        final Account account = optionalAccount.orElseThrow(() -> new UsernameNotFoundException(username));
+        return new User(account.getUsername(), account.getPassword(), authorities());
+    }
+
+    private Collection<? extends GrantedAuthority> authorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 }
