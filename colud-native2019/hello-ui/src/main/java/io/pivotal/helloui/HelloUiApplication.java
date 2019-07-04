92b7ea80a14c6c64ee4493296bed9a28e079c0d1
siyoon <siyoon210@gmail.com>
2019-07-04 11:49:28 +0900

    피보탈 네이티브 클라우드 데이 2019 핸즈온 두번째 프로젝트 생성 (hello-ui)

---
+++ colud-native2019/hello-ui/src/main/java/io/pivotal/helloui/HelloUiApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.helloui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloUiApplication.class, args);
+    }
+
+}
