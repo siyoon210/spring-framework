a56044399abbc3befc20907a27b71afe2a2d1500
siyoon <siyoon210@gmail.com>
2019-07-04 10:18:13 +0900

    피보탈 클라우드 네이티브 데이 2019 행상 오전 핸즈온 프로젝트 생성

---
+++ colud-native2019/hello-server/src/main/java/io/pivotal/helloserver/HelloServerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+
+}
