126822a29e896f5cc179a75f3455fad4d02e6eb7
siyoon <siyoon210@gmail.com>
2019-07-04 11:06:04 +0900

    Config Server 사용하기
    요청 매핑, 프로퍼티 추가

--- colud-native2019/hello-server/src/main/java/io/pivotal/helloserver/HelloController.java
+++ colud-native2019/hello-server/src/main/java/io/pivotal/helloserver/HelloController.java
@@ -1,10 +1,22 @@
 package io.pivotal.helloserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${greeting}")
+    String greeting;
+
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String toWho) {
+        final String result = String.format(greeting, toWho);
+        System.out.println(result);
+        return "Hello World!";
+    }
+
     @RequestMapping("/")
     public String helloWorld() {
         return "Hello World!";
