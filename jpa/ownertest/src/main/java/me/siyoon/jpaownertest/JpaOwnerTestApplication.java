2129035c11639759a6c6c55b883bf8ce78ac6f4f
siyoon <siyoon210@gmail.com>
2019-07-07 11:11:45 +0900

    JPA 연관관계 주인 테스트용 프로젝트 생성

---
+++ jpa/ownertest/src/main/java/me/siyoon/jpaownertest/JpaOwnerTestApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.jpaownertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOwnerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaOwnerTestApplication.class, args);
+    }
+
+}
---
+++ jpa/ownertest/src/test/java/me/siyoon/jpaownertest/JpaOwnerTestApplicationTests.java
@@ -0,0 +1,16 @@
+package me.siyoon.jpaownertest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaOwnerTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
