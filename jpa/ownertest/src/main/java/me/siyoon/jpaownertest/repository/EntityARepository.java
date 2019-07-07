2726ab2b3d3c1f4697ab1b194bd71dece568d8d6
siyoon <siyoon210@gmail.com>
2019-07-07 11:46:33 +0900

    JPA레퍼지토리를 상속받은 레퍼지토리 생성

---
+++ jpa/ownertest/src/main/java/me/siyoon/jpaownertest/repository/EntityARepository.java
@@ -0,0 +1,7 @@
+package me.siyoon.jpaownertest.repository;
+
+import me.siyoon.jpaownertest.entity.EntityA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntityARepository extends JpaRepository<EntityA, Long> {
+}
---
+++ jpa/ownertest/src/main/java/me/siyoon/jpaownertest/repository/EntityBRepository.java
@@ -0,0 +1,7 @@
+package me.siyoon.jpaownertest.repository;
+
+import me.siyoon.jpaownertest.entity.EntityB;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntityBRepository extends JpaRepository<EntityB, Long> {
+}
