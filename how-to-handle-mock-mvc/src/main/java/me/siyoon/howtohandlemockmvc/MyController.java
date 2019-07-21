4b80b72ddc3eb5107ecf3dace65fd62fbd672d88
siyoon <siyoon210@gmail.com>
2019-07-21 16:11:44 +0900

    @AutoConfigureMockMvc로 주입받아서 테스트하기

--- how-to-handle-mock-mvc/src/main/java/me/siyoon/howtohandlemockmvc/MyController.java
+++ how-to-handle-mock-mvc/src/main/java/me/siyoon/howtohandlemockmvc/MyController.java
@@ -2,11 +2,12 @@ package me.siyoon.howtohandlemockmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
     @GetMapping("/hello")
-    public String hello() {
+    public @ResponseBody String hello() {
         return "hello";
     }
 }
--- how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
+++ how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
@@ -2,17 +2,27 @@ package me.siyoon.howtohandlemockmvc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class MyControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
-    public void hello() {
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk());
     }
 }
