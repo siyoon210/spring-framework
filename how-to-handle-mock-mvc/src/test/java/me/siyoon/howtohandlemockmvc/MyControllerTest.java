f1705e5abfd0a155c91b6ce7028eb5264a6502f8
siyoon <siyoon210@gmail.com>
2019-07-21 17:22:03 +0900

    WebTestClient로 변경

--- how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
+++ how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
@@ -3,31 +3,22 @@ package me.siyoon.howtohandlemockmvc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MyControllerTest {
-    MockMvc mockMvc;
+    @Autowired
+    WebTestClient webTestClient;
 
-    public MyControllerTest() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new MyController()).build();
-    }
 
-    @Test
-    public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk());
-    }
+@Test
+public void helloTest() throws Exception {
+    webTestClient.put().uri("/hello")
+            .exchange()
+            .expectStatus();
 }
+}
+
