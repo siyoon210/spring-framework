d57681d20dec489baef36172e7f5aadd545fcd84
siyoon <siyoon210@gmail.com>
2019-07-22 09:27:12 +0900

    MockMvc로 롤백 (스프링 5이상만 웹테스트 클라이언트를 지원하기 때문에 필수로 알아야함)

--- how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
+++ how-to-handle-mock-mvc/src/test/java/me/siyoon/howtohandlemockmvc/MyControllerTest.java
@@ -3,22 +3,29 @@ package me.siyoon.howtohandlemockmvc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MyControllerTest {
     @Autowired
-    WebTestClient webTestClient;
+    private MockMvc mockMvc;
 
 
-@Test
-public void helloTest() throws Exception {
-    webTestClient.put().uri("/hello")
-            .exchange()
-            .expectStatus();
-}
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
 
