80551b214ce404c41cf30cc406dcdd45c3d629e9
siyoon <siyoon210@gmail.com>
2019-07-26 09:20:37 +0900

    동적 빈 주입이 가능한지 테스트해보는 프로젝트

---
+++ live-bean-setter-test/src/main/java/me/siyoon/livebeansettertest/LiveBeanSetterTestApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.livebeansettertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBeanSetterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveBeanSetterTestApplication.class, args);
+    }
+
+}
