f43613115fe16dd7cd27ff01ffff2a2eea0073ba
siyoon <siyoon210@gmail.com>
2019-07-27 09:38:51 +0900

    맵으로 하면 클래스 이름이 키로 들어간다!

--- live-bean-setter-test/src/main/java/me/siyoon/livebeansettertest/MusicPlayer.java
+++ live-bean-setter-test/src/main/java/me/siyoon/livebeansettertest/MusicPlayer.java
@@ -4,14 +4,20 @@ import lombok.RequiredArgsConstructor;
 import me.siyoon.livebeansettertest.Music.Music;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
 import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
 public class MusicPlayer {
-    private final Set<Music> music;
+//    private final Set<Music> music;
+    private final Map<String, Music> music;
 
     public void playingAllMusic() {
-        music.forEach(Music::playing);
+        music.forEach((key, value) -> {
+            System.out.println("key = " + key);
+            value.playing();
+        });
+
     }
 }
