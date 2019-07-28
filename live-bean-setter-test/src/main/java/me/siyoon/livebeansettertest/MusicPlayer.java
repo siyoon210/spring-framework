f59284f055079d331c4ec288dc6e11fd3cd58974
siyoon <siyoon210@gmail.com>
2019-07-28 10:28:21 +0900

    리스트도 되네, 순서는 무슨 기준이지?

--- live-bean-setter-test/src/main/java/me/siyoon/livebeansettertest/MusicPlayer.java
+++ live-bean-setter-test/src/main/java/me/siyoon/livebeansettertest/MusicPlayer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.siyoon.livebeansettertest.Music.Music;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,13 +12,15 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class MusicPlayer {
 //    private final Set<Music> music;
-    private final Map<String, Music> music;
+    private final List<Music> music;
+//    private final Map<String, Music> music;
 
     public void playingAllMusic() {
-        music.forEach((key, value) -> {
-            System.out.println("key = " + key);
-            value.playing();
-        });
+//        music.forEach((key, value) -> {
+//            System.out.println("key = " + key);
+//            value.playing();
+//        });
 
+        music.forEach(Music::playing);
     }
 }
