9447ff2523fa6473f268308886bc66c521e3181f
siyoon <siyoon210@gmail.com>
2019-08-01 09:53:30 +0900

    발급받은 auth code 로 Access Token 발급받는 url 주소 생성하기

--- tistory-api-test/src/main/java/me/siyoon/tistoryapitest/TistoryApiClient.java
+++ tistory-api-test/src/main/java/me/siyoon/tistoryapitest/TistoryApiClient.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class TistoryApiClient {
     private static final String APP_ID = "76a2f883a9b9caf68d6c7223481a44fd"; //client id 라고도 한다.
     private static final String CALL_BACK_URI = "http://localhost:5000/callback"; //redirect uri 라고도 한다.
+    private static final String SECRET_KEY = "76a2f883a9b9caf68d6c7223481a44fd87ee4dacbb700fce5f22a3bac74494189e023038";
     private final RestTemplate restTemplate;
 
     public TistoryApiClient() {
@@ -38,6 +39,25 @@ public class TistoryApiClient {
     @ResponseBody
     public String callBack(@RequestParam(required = false) String code) {
         System.out.println("code = " + code);
+        getAccessToekn(code);
         return code;
+    }
+
+    /**
+     * 전달받은 Auth Code 로 Access Token 발급받기
+     * https://tistory.github.io/document-tistory-apis/auth/authorization_code.html
+     */
+    private void getAccessToekn(String code) {
+        final String url = "https://www.tistory.com/oauth/" + "access_token"
+                + "?client_id=" + APP_ID
+                + "&client_secret" + SECRET_KEY
+                + "&redirect_uri=" + CALL_BACK_URI
+                + "&code=" + code
+                + "&grant_type=authorization_code"; //Implicit 방식과 구분하기 위해 사용하며 항상 authorization_code를 사용합니다.
+
+        System.out.println("url = " + url);
+
+//        final Object forObject = restTemplate.getForObject(url, Object.class);
+//        System.out.println("forObject = " + forObject);
     }
 }
