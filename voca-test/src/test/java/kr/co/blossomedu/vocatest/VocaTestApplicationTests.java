aafd39e4c210b6f536867b115aaf8de9c7f1f62d
siyoon <siyoon210@gmail.com>
2019-08-06 21:30:23 +0900

    프로젝트 생성

---
+++ voca-test/src/test/java/kr/co/blossomedu/vocatest/VocaTestApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.blossomedu.vocatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VocaTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
