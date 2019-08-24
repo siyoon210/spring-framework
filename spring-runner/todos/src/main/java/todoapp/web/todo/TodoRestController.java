31fd5620dbc66cc9bd5772b3ec4e1b66963cea83
siyoon <siyoon210@gmail.com>
2019-08-24 16:31:19 +0900

    todo 등록수정삭제 api 기능 구현

--- spring-runner/todos/src/main/java/todoapp/web/todo/TodoRestController.java
+++ spring-runner/todos/src/main/java/todoapp/web/todo/TodoRestController.java
@@ -1,24 +1,72 @@
 package todoapp.web.todo;
 
-import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import todoapp.core.todos.application.TodoEditor;
 import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 
 @RestController
 public class TodoRestController {
-	private TodoFinder finder;
+	private final Logger log = LoggerFactory.getLogger(TodoRestController.class);
+	private final TodoFinder finder;
+	private final TodoEditor editor;
 	
-	public TodoRestController(TodoFinder finder) {
+	public TodoRestController(TodoFinder finder, TodoEditor editor) {
 		this.finder = finder;
+		this.editor = editor;
 	}
 	
 	@GetMapping("/api/todos")
 	public List<Todo> todos(){
 		return finder.getAll();
+	}
+	
+	@PostMapping("/api/todos")
+	public void create(@RequestBody TodoWriteCommand command) {
+		log.debug("command.title: '{}'", command.getTitle());
+		editor.create(command.getTitle());
+	}
+	
+	@PutMapping("/api/todos/{id}")
+	public void update(@PathVariable("id") Long id, @RequestBody TodoWriteCommand command) {
+		log.debug("command.title: '{}', command.completed: '{}'", command.getTitle());
+		editor.update(id, command.getTitle(), command.getCompleted());
+	}
+	
+	@DeleteMapping("/api/todos/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		editor.delete(id);
+	}
+	
+	public static class TodoWriteCommand{
+		private String title;
+		private boolean completed;
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public boolean getCompleted() {
+			return completed;
+		}
+
+		public void setCompleted(boolean completed) {
+			this.completed = completed;
+		}
 	}
 }
