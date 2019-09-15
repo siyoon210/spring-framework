9677cc40ca4a43f018ef3d214a19f892fa5def2f
siyoon <siyoon210@gmail.com>
2019-09-15 15:44:54 +0900

    GraphQL 연습용 프로젝트 생성 (Baeldung)

---
+++ graphql/baeldung-example/src/main/java/me/siyoon/graphql/baeldungexample/BaeldungExampleApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.graphql.baeldungexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeldungExampleApplication.class, args);
+    }
+
+}
---
+++ graphql/baeldung-example/src/test/java/me/siyoon/graphql/baeldungexample/BaeldungExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package me.siyoon.graphql.baeldungexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaeldungExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
