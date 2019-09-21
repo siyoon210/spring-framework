dc3a9ad907e907b624229e07b022b4be833994b2
siyoon <siyoon210@gmail.com>
2019-09-21 15:02:21 +0900

    import 최적화

--- graphql/baeldung-example/src/main/java/me/siyoon/graphql/baeldungexample/graphql/MyMutation.java
+++ graphql/baeldung-example/src/main/java/me/siyoon/graphql/baeldungexample/graphql/MyMutation.java
@@ -2,7 +2,6 @@ package me.siyoon.graphql.baeldungexample.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import lombok.RequiredArgsConstructor;
-import me.siyoon.graphql.baeldungexample.domain.Author;
 import me.siyoon.graphql.baeldungexample.domain.Post;
 import me.siyoon.graphql.baeldungexample.dto.PostResponse;
 import me.siyoon.graphql.baeldungexample.repository.AuthorRepository;
