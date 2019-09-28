a2e4360d2fce1a8e5df81b529d26f6607136137f
siyoon <siyoon210@gmail.com>
2019-09-28 20:19:54 +0900

    DTO List to Set 연습용 프로젝트 생성

---
+++ practice/dto-list-to-set/src/main/java/me/siyoon/dtolisttosettest/DtoListToSetTestApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.dtolisttosettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoListToSetTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtoListToSetTestApplication.class, args);
+    }
+
+}
