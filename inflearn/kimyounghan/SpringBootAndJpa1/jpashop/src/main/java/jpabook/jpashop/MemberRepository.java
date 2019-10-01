972a30e479dfe5a0e5f3140e932a1392ac093a7f
siyoon <siyoon210@gmail.com>
2019-10-01 23:03:17 +0900

    Member 엔티티 레퍼지토리 생성, 테스트코드 생성

---
+++ inflearn/kimyounghan/SpringBootAndJpa1/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Long save(Member member) {
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+}
---
+++ inflearn/kimyounghan/SpringBootAndJpa1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("Siyoon");
+
+        //when
+        final Long saveId = memberRepository.save(member);
+        final Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
