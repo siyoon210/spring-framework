cb008f97e580fe917137a22738f0f6bd3a1401d7
siyoon <siyoon210@gmail.com>
2019-11-04 09:26:31 +0900

    AJAX로 파일 업로드하기 연습용 프로젝트 생성

---
+++ practice/ajax-file-upload/src/main/java/me/siyoon/ajaxfileupload/AjaxFileUploadApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.ajaxfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxFileUploadApplication.class, args);
+    }
+
+}
---
+++ practice/ajax-file-upload/src/test/java/me/siyoon/ajaxfileupload/AjaxFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.ajaxfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
