9c2a593ba2ba3fe05260b64935c42cca61b68793
siyoon <siyoon210@gmail.com>
2019-11-04 09:30:30 +0900

    html 페이지 폼 및 js (+ 미커밋 파일 같이)

--- inflearn/kimyounghan/SpringBootAndJpa1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ inflearn/kimyounghan/SpringBootAndJpa1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,5 +30,6 @@ public class MemberRepositoryTest {
         //then
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
     }
 }
