ea6d88d78c2e4423c9e8d6c91d4c77758aa9501c
siyoon <siyoon210@gmail.com>
2019-11-07 09:27:40 +0900

    async 연습용 프로젝트 생성

---
+++ practice/async-test/src/main/java/me/siyoon/asynctest/AsyncTestApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.asynctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTestApplication.class, args);
+    }
+
+}
---
+++ practice/async-test/src/test/java/me/siyoon/asynctest/AsyncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.asynctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
