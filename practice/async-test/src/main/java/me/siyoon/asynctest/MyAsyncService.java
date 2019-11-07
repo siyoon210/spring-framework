f80ab01265795361b58814139f51b89fb70a6f9c
siyoon <siyoon210@gmail.com>
2019-11-08 08:59:09 +0900

    @Async 메소드의 반환값이 필요할 때 Future<>타입으로 반환받기

--- practice/async-test/src/main/java/me/siyoon/asynctest/MyAsyncService.java
+++ practice/async-test/src/main/java/me/siyoon/asynctest/MyAsyncService.java
@@ -2,17 +2,22 @@ package me.siyoon.asynctest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
 
 @Service
 @Slf4j
 public class MyAsyncService {
     @Async(value = "myTaskExecutor")
-    public void myAsyncMethod() throws InterruptedException {
+    public Future<String> myAsyncMethod() throws InterruptedException {
         for (int i = 0; i < 3; i++) {
             Thread.sleep(1000);
             log.info("Async! '{}', Thread Name : {}", i, Thread.currentThread().getName());
         }
+
+        return AsyncResult.forValue("끝!! ~(-ㅇ- ~)");
     }
 
     public void mySyncMethod() throws InterruptedException {
--- practice/async-test/src/main/java/me/siyoon/asynctest/MyController.java
+++ practice/async-test/src/main/java/me/siyoon/asynctest/MyController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -13,13 +16,14 @@ public class MyController {
     private final MyAsyncService myAsyncService;
 
     @GetMapping("/async")
-    public String async() throws InterruptedException {
-        for (int i = 0; i < 3; i++) {
-            log.info("Controller Thread Name: '{}'", Thread.currentThread().getName());
-            myAsyncService.myAsyncMethod();
-        }
+    public String async() throws InterruptedException, ExecutionException {
+        log.info("Controller Thread Name: '{}'", Thread.currentThread().getName());
+        final Future<String> stringFuture1 = myAsyncService.myAsyncMethod();
+        final Future<String> stringFuture2 = myAsyncService.myAsyncMethod();
+        final Future<String> stringFuture3 = myAsyncService.myAsyncMethod();
 
-        System.out.println("------------------");
+
+        System.out.println("------------------" + stringFuture1.get() + stringFuture2.get() + stringFuture3.get());
         return "done";
     }
 
