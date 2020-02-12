466ecc2267e617d3b2261c9e74b6560749072232
siyoon <siyoon210@gmail.com>
2020-02-12 09:33:37 +0900

    지하철역 생성하기 인수테스트 예제 작성

--- atdd-subway-path-master/src/test/java/atdd/station/StationAcceptanceTest.java
+++ atdd-subway-path-master/src/test/java/atdd/station/StationAcceptanceTest.java
@@ -1,8 +1,36 @@
 package atdd.station;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class StationAcceptanceTest {
-    private static final Logger logger = LoggerFactory.getLogger(StationAcceptanceTest.class);
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @DisplayName("지하철 역 생성요청하기")
+    public void createStation() {
+        String stationName = "강남역";
+        String inputJson = "{\"name\":\""+stationName+"\"}";
+
+        webTestClient.post().uri("/stations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(inputJson), String.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectHeader().exists("Location")
+                .expectBody().jsonPath("$.name").isEqualTo(stationName);
+    }
+
 }
