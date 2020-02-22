efb5b2ae256213146b4bd93f06e30a68fe5179d2
siyoon <siyoon210@gmail.com>
2020-02-22 16:00:18 +0900

    심플 채팅 프로젝트 생성

---
+++ simple-chat/src/main/java/me/siyoon/simplechat/SimpleChatApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatApplication.class, args);
+	}
+
+}
---
+++ simple-chat/src/test/java/me/siyoon/simplechat/SimpleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.simplechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
