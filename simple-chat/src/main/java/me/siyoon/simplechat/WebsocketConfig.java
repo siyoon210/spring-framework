989414fa5e87d3ab0daa2a876ce192618873c15b
siyoon <siyoon210@gmail.com>
2020-02-22 16:05:44 +0900

    WebSocketMessageBrokerConfigurer 설정

---
+++ simple-chat/src/main/java/me/siyoon/simplechat/WebsocketConfig.java
@@ -0,0 +1,22 @@
+package me.siyoon.simplechat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/websocket");
+        registry.enableSimpleBroker("/topic");
+    }
+}
