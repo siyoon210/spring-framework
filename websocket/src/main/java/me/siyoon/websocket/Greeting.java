15db482bd04cd935d960ac4581f134c029e59436
siyoon <siyoon210@gmail.com>
2020-02-22 13:30:54 +0900

    웹소켓 요청 응답용 도메인 클래스 생성

---
+++ websocket/src/main/java/me/siyoon/websocket/Greeting.java
@@ -0,0 +1,12 @@
+package me.siyoon.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Greeting {
+    private String content;
+}
---
+++ websocket/src/main/java/me/siyoon/websocket/HelloMessage.java
@@ -0,0 +1,14 @@
+package me.siyoon.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
