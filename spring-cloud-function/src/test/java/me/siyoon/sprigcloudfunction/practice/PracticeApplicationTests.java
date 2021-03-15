eb03f9dbdc4bc4ed77c39d5643aa74919013e53b
siyoon210 <siyoon210@gmail.com>
2021-03-15 11:09:17 +0900

    init spring cloud fuction sample project

---
+++ spring-cloud-function/src/test/java/me/siyoon/sprigcloudfunction/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.sprigcloudfunction.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
