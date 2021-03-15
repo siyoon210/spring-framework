6944a66d48a760b8a50ef0b1b8867f4312fd3d21
siyoon210 <siyoon210@gmail.com>
2021-03-15 11:18:07 +0900

    로컬 테스트를 위한 spring-cloud-starter-function-web 의존추가, 예제코드 추가

--- spring-cloud-function/src/main/java/me/siyoon/sprigcloudfunction/practice/PracticeApplication.java
+++ spring-cloud-function/src/main/java/me/siyoon/sprigcloudfunction/practice/PracticeApplication.java
@@ -2,7 +2,14 @@ package me.siyoon.sprigcloudfunction.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.function.Function;
+
+/**
+ * 예제 참고
+ * https://www.baeldung.com/spring-cloud-function
+ */
 @SpringBootApplication
 public class PracticeApplication {
 
@@ -10,4 +17,8 @@ public class PracticeApplication {
         SpringApplication.run(PracticeApplication.class, args);
     }
 
+    @Bean
+    public Function<String, String> reverseString() {
+        return value -> new StringBuilder(value).reverse().toString();
+    }
 }
