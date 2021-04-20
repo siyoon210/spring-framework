94982105390cdc5ca028b13cfd30dbaa1259367f
siyoon210 <siyoon210@gmail.com>
2021-04-20 20:48:43 +0900

    spring cloud function gralde용 프로젝트

---
+++ spring-cloud-function-lambda/src/main/java/me/siyoon/springcloudfunctionlambda/Greeter.java
@@ -0,0 +1,14 @@
+package me.siyoon.springcloudfunctionlambda;
+
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class Greeter implements Function<String, String> {
+
+    @Override
+    public String apply(String s) {
+        return "Hello " + s + ", and welcome to Spring Cloud Function!!!";
+    }
+}
