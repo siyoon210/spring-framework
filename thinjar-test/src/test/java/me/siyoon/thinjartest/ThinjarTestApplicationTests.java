ee36737e1d137281f7d9d145978e4beac32e84df
siyoon210 <siyoon210@gmail.com>
2021-04-21 09:10:13 +0900

    thinjar 연습용 프로젝트

---
+++ thinjar-test/src/test/java/me/siyoon/thinjartest/ThinjarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.thinjartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinjarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
