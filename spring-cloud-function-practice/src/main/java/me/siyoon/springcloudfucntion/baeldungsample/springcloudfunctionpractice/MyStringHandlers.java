2333a551bfec0ca5a908d4df95b541a52e3ea9c1
siyoon210 <siyoon210@gmail.com>
2021-05-08 08:46:37 +0900

    aws 연습용 프로젝트 삭제

--- spring-cloud-function-practice/src/main/java/me/siyoon/springcloudfucntion/baeldungsample/springcloudfunctionpractice/MyStringHandlers.java
+++
@@ -1,7 +0,0 @@
-package me.siyoon.springcloudfucntion.baeldungsample.springcloudfunctionpractice;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
-
-}
