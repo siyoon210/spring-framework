e1b0a436b05f2a0268b43a41e057f1cd41f41eab
siyoon210 <siyoon210@gmail.com>
2021-05-08 10:40:46 +0900

    AWS Lambda 테스트용 프로젝트 생성, (jar가 아닌 bootJar로 만든다! ㅠㅠ)

---
+++ springboot-lambda-test/src/main/java/me/siyoon/springbootlambdatest/SpringbootLambdaTestApplication.java
@@ -0,0 +1,20 @@
+package me.siyoon.springbootlambdatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class SpringbootLambdaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLambdaTestApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> reverseString() {
+        return value -> new StringBuilder(value).reverse().toString();
+    }
+}
---
+++ springboot-lambda-test/src/test/java/me/siyoon/springbootlambdatest/SpringbootLambdaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.siyoon.springbootlambdatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLambdaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
