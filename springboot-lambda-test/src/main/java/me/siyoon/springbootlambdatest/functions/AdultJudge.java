ced0c90fff8e74e3fdecf3b16077235ab4f5056f
siyoon210 <siyoon210@gmail.com>
2021-05-24 10:29:41 +0900

    cloud-web 없이도 로컬테스트 가능

--- springboot-lambda-test/src/main/java/me/siyoon/springbootlambdatest/functions/AdultJudge.java
+++ springboot-lambda-test/src/main/java/me/siyoon/springbootlambdatest/functions/AdultJudge.java
@@ -1,22 +1,17 @@
 package me.siyoon.springbootlambdatest.functions;
 
 import me.siyoon.springbootlambdatest.component.LowerCaseHelper;
-import me.siyoon.springbootlambdatest.dto.UserRequest;
 import me.siyoon.springbootlambdatest.dto.AdultJudgeResponse;
+import me.siyoon.springbootlambdatest.dto.UserRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
+@Component
 public class AdultJudge implements Function<UserRequest, AdultJudgeResponse> {
-    private final LowerCaseHelper lowerCaseHelper;
-
-    public AdultJudge(LowerCaseHelper lowerCaseHelper) {
-        this.lowerCaseHelper = lowerCaseHelper;
-    }
-
     @Override
     public AdultJudgeResponse apply(UserRequest request) {
-        String name = lowerCaseHelper.toLowerCase(request.getName());
+        String name = request.getName();
 
         if (request.getAge() > 20) {
             return new AdultJudgeResponse(name + " is Adult!");
